478f9655e219e7aba3ffb24f3f1610890dc34eaf
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-11-09 16:56:24 +0100

    Coinbase url method creation

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,4 +1,14 @@
 export class AuthService {
+  ALLOWED_TO_UNSIGNED = [
+    '/signin'
+  ];
+
+  COINBASE_AUTH_BASE_URL = 'https://www.coinbase.com/oauth/authorize';
+  COINBASE_CLIENT_ID = '8a7cbffab1d011558b80731428985953b5758308fa1db27a3548420b4e6abbfa';
+  COINBASE_REDIRECT_URI = 'https://nostradamusbot.com/callback';
+  COINBASE_RESPONSE_TYPE = 'code';
+  COINBASE_SCOPE = 'wallet:user:read,wallet:user:email';
+
   loggedIn = false;
 
   isAuthenticated() {
@@ -16,6 +26,15 @@ export class AuthService {
 
   logout() {
     this.loggedIn = false;
+  }
+
+  getCoinbaseAuthCodeRequestURL() {
+    console.log(encodeURI(this.COINBASE_AUTH_BASE_URL + '?'
+      + '&' + 'client_id=' + this.COINBASE_CLIENT_ID
+      + '&' + 'redirect_uri=' + this.COINBASE_REDIRECT_URI
+      + '&' + 'response_type=' + this.COINBASE_RESPONSE_TYPE
+      + '&' + 'scope=' + this.COINBASE_SCOPE
+    ));
   }
 }
 
--- frontend/src/app/auth/signin/signin.component.ts
+++ frontend/src/app/auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getCoinbaseAuthCodeRequestURL();
   }
 
 }
