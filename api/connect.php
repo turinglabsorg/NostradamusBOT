e801d74c2def64fc7cf4689099101cd7915a34fa
Sebastiano Cataudo <sebastiano@wearefuturing.com>
2017-11-13 10:12:38 +0100

    api

---
+++ api/connect.php
@@ -0,0 +1,6 @@
+<?php
+	$hostname_connect = "localhost";
+	$database_connect = "nostradamus_app";
+	$username_connect = "nostradamus_app";
+	$password_connect = "Jhfh80%2";
+?>
--- api/index.php
+++ api/index.php
@@ -1,1 +1,22 @@
-<?php echo 'Hi'; ?>
+<?php	
+	error_reporting(E_ALL); ini_set('display_errors', 'On'); 
+	header("Access-Control-Allow-Orgin: *");
+    header("Access-Control-Allow-Methods: *");
+    header("Content-Type: application/json");
+    
+    //declarations
+	require_once('connect.php');
+	include('core.php');
+	include('api.class.php');
+	//
+	
+	//zcvY59zL2MvZ4NnZzd3cl5SaqQ==
+	
+	try {
+	    $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+	    echo $API->processAPI();
+	} catch (Exception $e) {
+	    echo json_encode(Array('error' => $e->getMessage()));
+	}
+
+?>
