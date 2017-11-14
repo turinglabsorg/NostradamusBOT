36ec03eaa13675ab44cd4eb2cdae928da10669f7
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-11-14 18:21:22 +0100

    public router in navbar

--- app/src/app/navbar/navbar.component.ts
+++ app/src/app/navbar/navbar.component.ts
@@ -9,7 +9,8 @@ import {AuthService} from '../auth/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(private router: Router, public authService: AuthService) {
+
   }
 
   ngOnInit() {
