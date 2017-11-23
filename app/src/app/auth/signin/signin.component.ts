52caaa4362e63c01108144cb55e80d5982ccf813
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-11-23 17:54:00 +0100

    scope variable not used deleted

--- app/src/app/auth/signin/signin.component.ts
+++ app/src/app/auth/signin/signin.component.ts
@@ -14,7 +14,6 @@ export class SigninComponent implements OnInit, OnDestroy {
     email: '',
     password: ''
   };
-  scope = 'wallet:user:read,wallet:user:email,wallet:accounts:create';
 
   constructor(private authService: AuthService, private apiService: ApiService, private router: Router) {
   }
