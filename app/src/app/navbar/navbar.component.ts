c92253c79bf6539b0b05b259e8ba32db929ec9ea
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-11-28 13:14:37 +0100

    fix #3 - navbar centered and responsive

--- app/src/app/navbar/navbar.component.ts
+++ app/src/app/navbar/navbar.component.ts
@@ -8,9 +8,14 @@ import {AuthService} from '../auth/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isCollapsed = true;
 
   constructor(private router: Router, public authService: AuthService) {
 
+  }
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
   }
 
   ngOnInit() {
