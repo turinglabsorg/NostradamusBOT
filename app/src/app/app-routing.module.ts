1b96bc2fae48b7286d39845247e13b4afde49318
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-12-06 10:18:55 +0100

    routing code cleaning

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {AuthComponent} from './auth/auth.component';
 import {SigninComponent} from './auth/signin/signin.component';
 import {AuthGuard} from './auth/auth-guard.service';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {SettingsComponent} from './settings/settings/settings.component';
-import {RulesComponent} from './rules/rules.component';
 import {ActionsComponent} from './actions/actions-list/actions-list.component';
 import {RuleEditComponent} from './rules/rule-edit/rule-edit.component';
-import {RuleDetailComponent} from './rules/rule-detail/rule-detail.component';
 import {RulesListComponent} from './rules/rules-list/rules-list.component';
 
 const appRoutes: Routes = [
@@ -17,16 +15,9 @@ const appRoutes: Routes = [
   {path: 'callback', component: AuthComponent},
   {path: 'signin', component: SigninComponent, canActivate: [AuthGuard]},
   {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
-  { path: 'rules', component: RulesListComponent, canActivate: [AuthGuard]},
-  { path: 'rules/new', component: RuleEditComponent, canActivate: [AuthGuard]},
-  // { path: 'rules/:id', component: RuleDetailComponent, canActivate: [AuthGuard]},
-  { path: 'rules/:id/edit', component: RuleEditComponent, canActivate: [AuthGuard]},
-  // { path: 'rules', component: RulesComponent, canActivate: [AuthGuard], children: [
-  //   { path: '', component: RuleDetailComponent },
-  //   { path: 'new', component: RuleEditComponent },
-  //   { path: ':id', component: RuleDetailComponent },
-  //   { path: ':id/edit', component: RuleEditComponent },
-  // ] },
+  {path: 'rules', component: RulesListComponent, canActivate: [AuthGuard]},
+  {path: 'rules/new', component: RuleEditComponent, canActivate: [AuthGuard]},
+  {path: 'rules/:id/edit', component: RuleEditComponent, canActivate: [AuthGuard]},
   {path: 'actions', component: ActionsComponent, canActivate: [AuthGuard]}
 
 ];
