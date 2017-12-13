350d0909bb5c1e0e9473d71c55871d6f3d3c0314
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-12-13 09:38:00 +0100

    wallet balance bug fix

--- app/src/app/dashboard/wallet/wallet.component.ts
+++ app/src/app/dashboard/wallet/wallet.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ApiService} from '../../api/api.service';
 import {AuthService} from '../../auth/auth.service';
 import {LangService} from '../../lang/lang.service';
+import {isUndefined} from 'util';
 
 @Component({
   selector: 'app-wallet',
@@ -24,13 +25,15 @@ export class WalletComponent implements OnInit {
         if (this.apiService.isSuccessfull(rawResponse)) {
           const response: string = this.apiService.parseAPIResponse(rawResponse);
           console.log(response);
-          this.balance = parseFloat(response).toFixed(4);
-          this.showBalanceOrButton = true;
+          this.balance = response;
+          if (isUndefined(this.balance) || this.balance == null) {
+            this.balance = -1;
+          }
         } else {
           console.log('errore');
           this.balance = -1;
-          this.showBalanceOrButton = true;
         }
+        this.showBalanceOrButton = true;
       }
     );
   }
