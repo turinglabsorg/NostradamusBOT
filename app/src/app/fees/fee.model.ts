2c1afabee98b8df2556b4426e7f91fd5e75be0eb
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2017-12-14 10:20:56 +0100

    fee section

---
+++ app/src/app/fees/fee.model.ts
@@ -0,0 +1,20 @@
+import {Base} from '../base.model';
+import {Rule} from '../rules/rule.model';
+import {Action} from '../actions/action.model';
+
+export class Fee extends Base {
+  public id: string = '';
+  public uuid_user: string = '';
+  public fee_date: string = '';
+  public id_action: string = '';
+  public id_rule: string = '';
+  public id_wallet: string = '';
+  public fee_paid: string = '';
+  public rule: Rule;
+  public action: Action;
+
+  get isPaid(): boolean {
+    return this.fee_paid === 'y';
+  }
+
+}
--- app/src/app/fees/fees.service.ts
+++ app/src/app/fees/fees.service.ts
@@ -1,16 +1,44 @@
 import {Injectable} from '@angular/core';
-import * as _ from 'lodash';
+import {Fee} from './fee.model';
+import {Rule} from '../rules/rule.model';
+import {Action} from '../actions/action.model';
 
 
 @Injectable()
 export class FeesService {
-  private fees: any[] = [];
+  private fees: Object;
+  private feesToPayPerMonth: Object;
 
   constructor() {
   }
 
-  setFees(fees) {
-    this.fees = _.remove(this.fees, _.isUndefined);
+  setFees(rawFees) {
+    this.fees = {};
+    this.feesToPayPerMonth = {};
+
+    for (let i = 0; i < Object.keys(rawFees).length; i++) {
+      const monthLabel = Object.keys(rawFees)[i];
+      this.feesToPayPerMonth[monthLabel] = rawFees[monthLabel]['to_pay'];
+      this.fees[monthLabel] = [];
+      for (let j = 0; j < rawFees[monthLabel]['fees'].length; j++) {
+        const _fee = new Fee();
+        _fee.fillFromJSON(rawFees[monthLabel]['fees'][j]);
+        _fee.rule = new Rule();
+        _fee.action = new Action();
+        _fee.rule.fillFromJSON(rawFees[monthLabel]['fees'][j]['rule']);
+        _fee.rule.wallet = _fee['wallet'];
+        _fee.action.fillFromJSON(rawFees[monthLabel]['fees'][j]['action']);
+        this.fees[monthLabel].push(_fee);
+      }
+    }
+
+    // /*fake data*/
+    // const fee = this.fees['2017-12'][0];
+    // this.fees['2017-12'].push(fee);
+    // this.fees['2017-11'] = [];
+    // this.fees['2017-11'].push(fee);
+    // this.fees['2017-11'].push(fee);
+    // this.fees['2017-11'].push(fee);
 
     console.log('--------------------------');
     console.log('Set Fees on Fees Service');
@@ -18,8 +46,16 @@ export class FeesService {
     console.log('--------------------------');
   }
 
-  getActions(): any[] {
+  getFees(): Object {
     return this.fees;
+  }
+
+  getFeesLabels(): string[] {
+    return Object.keys(this.fees);
+  }
+
+  getFeesTotalPerMonth(monthLabel: string) {
+    return this.feesToPayPerMonth[monthLabel];
   }
 
 }
